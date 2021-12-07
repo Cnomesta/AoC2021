b7ad104ed5799051f9656d03872f1a365afc692f
Cnomesta <36128569+Cnomesta@users.noreply.github.com>
2021-12-07 14:52:42 +0200

    AoC2021 - Day 7 - The Treachery of Whales
    Day 7 Completed

---
+++ Day07-TheTreacheryOfWhales/Part-2/TheTreacheryOfWhales2.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+
+const array = fs
+    .readFileSync('input.txt', {
+        encoding: 'utf-8'
+    })
+    .split(",")
+    .map(Number);
+
+function part1() {
+    let pos = 0;
+    let crab = 0;
+    let crabfuel = 0;
+    let leastFuel = 0;
+    let currentFuel = 0;
+    for (let i = 0; i <= array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            crab = array[j];
+            pos = i;
+            if (crab > pos) {
+                crabfuel = crab - pos;
+                currentFuel += crabfuel;
+            } else {
+                crabfuel = pos - crab;
+                currentFuel += crabfuel;
+            }
+            //console.log(pos, crab, currentFuel);
+        }
+        if (leastFuel > currentFuel || leastFuel === 0) {
+            leastFuel = currentFuel;
+            console.log(leastFuel, currentFuel);
+            currentFuel = 0;
+        } else {
+            console.log(leastFuel, currentFuel);
+            currentFuel = 0;
+        }
+    }
+}
+
+part1();
+
+function part2() {
+    let pos = 0;
+    let crab = 0;
+    let crabfuel = 0;
+    let leastFuel = 0;
+    let currentFuel = 0;
+    let crabdistance = 0;
+    for (let i = 0; i <= array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            crab = array[j];
+            pos = i;
+            if (crab > pos) {
+                crabdistance = crab - pos;
+                for (let k = crabdistance; k > 0; k--) {
+                    crabfuel += k;
+                }
+                currentFuel += crabfuel;
+                crabfuel = 0;
+                //console.log(crab, pos, crabdistance, crabfuel);
+            } else {
+                crabdistance = pos - crab;
+                for (let k = crabdistance; k > 0; k--) {
+                    crabfuel += k;
+                }
+                currentFuel += crabfuel;
+                crabfuel = 0;
+                //console.log(crab, pos, crabdistance, crabfuel);
+            }
+            //console.log(pos, crab, currentFuel);
+        }
+        if (leastFuel > currentFuel || leastFuel === 0) {
+            leastFuel = currentFuel;
+            console.log(leastFuel, currentFuel);
+            currentFuel = 0;
+        } else {
+            console.log(leastFuel, currentFuel);
+            currentFuel = 0;
+        }
+    }
+}
+
+part2();
